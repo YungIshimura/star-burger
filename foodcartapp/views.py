81afa847ec3ea2a1e3449ce2babd5c55fce289ff
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-09-23 18:38:08 +0200

    Rewrite data validation logic with DRF.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -2,10 +2,23 @@ from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
-import re
+from rest_framework.serializers import ModelSerializer
 
-from .models import Order, Product, Сustomer
+
+from .models import Order, Product, Customer
+
+
+class OrderSerializer(ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['product', 'quantity']
+
+
+class CustomerSerializer(ModelSerializer):
+    products = OrderSerializer(many=True, allow_empty=False)
+    class Meta:
+        model = Customer
+        fields = ['firstname', 'lastname', 'phonenumber', 'address', 'products']
 
 
 def banners_list_api(request):
@@ -62,55 +75,26 @@ def product_list_api(request):
 
 @api_view(['POST'])
 def register_order(request):
-    order = request.data
+    serializer = CustomerSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
 
-    if 'firstname' and 'lastname' and 'phonenumber' and 'address' not in order.keys():
-        content = {
-            'products, firstname, lastname, phonenumber, address: Обязательные поля, они не могут быть пустыми'}
+    order = serializer.validated_data['products']
 
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    customer = Customer.objects.create(
+        firstname=serializer.validated_data['firstname'],
+        lastname=serializer.validated_data['lastname'],
+        phonenumber=serializer.validated_data['phonenumber'],
+        address=serializer.validated_data['address'],
+    )
 
-    elif len(order['products']) == 0:
-        content = {'products: Этот список не может быть пустым'}
+    for products in order:
+        products = dict(products)
 
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
-
-    elif not re.match('^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$', order['phonenumber']):
-        content = {"phonenumber: Введен некорректный номер телефона"}
-
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
-
-    elif type(order['products']) is not list:
-        content = {
-            f'products: Ожидался list со значениями, но был получен {type(order["products"])}'
-        }
-
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
-
-    elif Product.objects.last().id < order['products'][0]['product']:
-        content = {
-            f'products: Недопустимый первичный ключ {order["products"][0]["product"]}'}
-
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
-
-    elif order['firstname'] is not str:
-        content = {'firstname: это не строка'}
-
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
-
-    else:
-        customer = Сustomer.objects.create(
-            name=order['firstname'],
-            surname=order['lastname'],
-            phone_number=order['phonenumber'],
-            address=order['address'],
+        Order.objects.create(
+            customer=customer,
+            product=products['product'],
+            quantity=products['quantity']
         )
-        for products in order['products']:
-            product = Product.objects.get(id=products['product'])
-            Order.objects.create(
-                customer=customer,
-                product=product,
-                quantity=products['quantity']
-            )
+
 
     return Response()
