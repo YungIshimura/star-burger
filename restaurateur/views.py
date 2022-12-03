7f9af0491e23d6d3b67e6db0d661056d3a06a2d8
YungIshimura <astr6.6.6@yandex.ru>
2022-12-03 20:59:22 +0000

    Fix error

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -146,7 +146,7 @@ def get_restaurants_geocode(order_item, restaurants):
                 GeoCode.objects.create(
                     latitude=rest_lat,
                     longitude=rest_lon,
-                    address=restaurant.address,
+                    address=restaurant.restaurant.address,
                     requested_at=datetime.now()
                 )
                 distance_to_restaurant = int(distance.distance(
