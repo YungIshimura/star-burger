42a90e8784fe64b490efb754d16e9e0a2394f283
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-12-19 17:50:50 +0200

    Update deploy_starburger.sh

--- deploy_starburger.sh
+++ deploy_starburger.sh
@@ -9,10 +9,10 @@ cd $project_path
 source venv/bin/activate
 git pull -q
 $python_path -m pip install -r requirements.txt
-npm install --dev
-./node_modules/.bin/parcel watch bundles-src/index.js --dist-dir bundles --public-url="./"
-$python_path manage.py collectstatic
-$python_path manage.py migrate
+npm install ci --dev --silent
+./node_modules/.bin/parcel watch bundles-src/index.js --dist-dir bundles --public-url="./" | killall node
+$python_path manage.py collectstatic --no-input
+$python_path manage.py migrate --no-input
 systemctl restart star-burger.service
 systemctl restart nginx.service
 curl -X POST https://api.rollbar.com/api/1/deploy -H "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" -d "environment=production&revision=$(git rev-parse HEAD)"
